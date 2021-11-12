0d60460340c96c89c5545ef67603f79ddba34d54
root <root@k8s7-200.zhangjihui.com.cn>
2021-11-12 13:24:16 +0800

    first commit

---
+++ dubbo-client/src/main/java/com/od/dubbotest/action/HelloAction.java
@@ -0,0 +1,24 @@
+package com.od.dubbotest.action;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.od.dubbotest.api.HelloService;
+
+@RestController
+@RequestMapping(value="/hello")
+public class HelloAction {
+	
+	@Reference HelloService helloService;
+	
+	@RequestMapping
+	public String say(String name) {
+		System.out.println("HelloAction接收到请求:"+name);
+		String str="<h1>这是Dubbo 消费者端(springboot)</h1>";
+		str+="<h2>学好kubernetes，走遍天下都不怕</h2>";
+		str+=helloService.hello("zhangsan");
+		System.out.println("HelloService返回到结果:"+str);
+		return str;
+	}
+}
